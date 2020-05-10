fdeb9171bb25660ce4bafa4f6646b2ac1bad7f02
corumov <ppplusa@gmail.com>
2020-05-10 21:48:13 +0200

    creating logic for showing info about given tv_series

--- data/queries.py
+++ data/queries.py
@@ -3,3 +3,13 @@ from data import data_manager
 
 def get_shows():
     return data_manager.execute_select('SELECT id, title FROM shows;')
+
+def get_given_show(show_id):
+    return data_manager.execute_select(f'SELECT * FROM shows WHERE id = %(id)s;', {"id":show_id})[0]
+
+
+def get_seasons_list(show_id):
+    return data_manager.execute_select(f'SELECT * FROM seasons WHERE show_id = %(id)s;', {"id": show_id})
+
+def get_genres_list(show_id):
+    return data_manager.execute_select(f'SELECT name FROM genres right join show_genres on genres.id = show_genres.genre_id WHERE show_id = %(id)s;', {"id": show_id})
--- main.py
+++ main.py
@@ -15,8 +15,24 @@ def design():
     return render_template('design.html')
 
 
+@app.route('/tv-show/<tv_show_id>')
+def show_given_series(tv_show_id=None):
+    series_id = int(tv_show_id)
+    show_details = queries.get_given_show(series_id)
+    seasons_details = queries.get_seasons_list(series_id)
+    genres = queries.get_genres_list(series_id)
+    print(show_details)
+    print(seasons_details)
+    print(queries.get_genres_list(series_id))
+
+    return render_template("tv_show_details.html", show_details=show_details, seasons_details=seasons_details, genres=genres, tv_show_id=tv_show_id)
+
 def main():
-    app.run(debug=True)
+    app.run(
+        debug=True,
+        # host='0.0.0.0',
+        port=7001
+    )
 
 
 if __name__ == '__main__':
