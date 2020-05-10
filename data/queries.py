19688d722b8e3ccd9c66a463120a2abe874ef3ef
baginski95 <bagiczny3@gmail.com>
2020-05-11 00:08:36 +0200

    tv show page

--- data/queries.py
+++ data/queries.py
@@ -4,12 +4,26 @@ from data import data_manager
 def get_shows():
     return data_manager.execute_select('SELECT id, title FROM shows;')
 
+
 def get_given_show(show_id):
-    return data_manager.execute_select(f'SELECT * FROM shows WHERE id = %(id)s;', {"id":show_id})[0]
+    return data_manager.execute_select(f'SELECT * FROM shows WHERE id = %(id)s;', {"id": show_id})[0]
 
 
 def get_seasons_list(show_id):
     return data_manager.execute_select(f'SELECT * FROM seasons WHERE show_id = %(id)s;', {"id": show_id})
 
+
+def get_given_season(season_id):
+    return data_manager.execute_select((f'SELECT * FROM seasons WHERE id = %(id)s;', {'id': season_id}))
+
+
 def get_genres_list(show_id):
-    return data_manager.execute_select(f'SELECT name FROM genres right join show_genres on genres.id = show_genres.genre_id WHERE show_id = %(id)s;', {"id": show_id})
+    return data_manager.execute_select(
+        f'SELECT name FROM genres right join show_genres on genres.id = show_genres.genre_id WHERE show_id = %(id)s;',
+        {"id": show_id})
+
+
+def get_given_episode(episode_id):
+    return data_manager.execute_select(
+        f'SELECT * FROM episodes WHERE id = %(id)s;',
+        {"id": episode_id})
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 from data import queries
 
 app = Flask('codecool_series')
@@ -20,12 +20,36 @@ def show_given_series(tv_show_id=None):
     series_id = int(tv_show_id)
     show_details = queries.get_given_show(series_id)
     seasons_details = queries.get_seasons_list(series_id)
-    genres = queries.get_genres_list(series_id)
-    print(show_details)
-    print(seasons_details)
-    print(queries.get_genres_list(series_id))
+    genres = ''
+    for genre in queries.get_genres_list(series_id):
+        genres += genre['name'] + ', '
 
-    return render_template("tv_show_details.html", show_details=show_details, seasons_details=seasons_details, genres=genres, tv_show_id=tv_show_id)
+    genres = genres.rstrip(', ')
+    tv_show_name = show_details['title']
+
+    return render_template("tv_show_details.html", show_details=show_details, seasons_details=seasons_details,
+                           genres=genres, tv_show_id=tv_show_id, tv_show_name=tv_show_name)
+
+
+@app.route('/tv-show/<tv_show_id>/<season_id>', methods=["get"])
+def get_season(tv_show_id=None, season_id=None):
+    single_season_details = queries.get_given_season(season_id)
+    tv_show_name = request.args.get()
+    season = single_season_details['season_number']
+    print(tv_show_name)
+    return render_template("tv_show_season.html", season=season, single_season_details=single_season_details,
+                           tv_show_name=tv_show_name, tv_show_id=tv_show_id)
+
+
+@app.route('/tv-show/<tv_show_id>/<season_id>/<episode_id>', methods=["get"])
+def get_episode(tv_show_id=None, season_id=None, episode_id=None):
+    episode_details = queries.get_given_episode(episode_id)
+    tv_show_name = request.args.get['tv_show_name']
+    season = request.args.get['season']
+    return render_template("tv_show_episode.html", season=season,
+                           tv_show_name=tv_show_name, tv_show_id=tv_show_id, season_id=season_id,
+                           episode_details=episode_details)
+
 
 def main():
     app.run(
